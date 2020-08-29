529fb8800014a5a9e98c62ebfe88e81d2bec51d7
tuan anh <canhazn@gmail.com>
2020-08-29 12:34:46 +0700

    Delete taggit

--- ruouhinh/urls.py
+++ ruouhinh/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path('taggit_autosuggest/', include("taggit_autosuggest.urls")),
+    path('summernote/', include('django_summernote.urls')),    
     path('', include('browser.urls')),
     path('', include('product.urls'))
 
