fe0a85dc20fea537541abd4ae6feffc3043d4f60
tuan anh <canhazn@gmail.com>
2020-09-04 08:55:26 +0700

    Add robots, sitemap file

--- ruouhinh/urls.py
+++ ruouhinh/urls.py
@@ -18,11 +18,22 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt",
+                             content_type="text/plain"),
+    ),
+    path(
+        "sitemap.xml",
+        TemplateView.as_view(template_name="sitemap.xml",
+                             content_type="text/xml"),
+    ),
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),    
+    path('summernote/', include('django_summernote.urls')),
     path('', include('browser.urls')),
     path('', include('product.urls'))
 
