0dc58a53a3c7e90df34718ed011cd34c6817d9c5
canhazn <canhazn@gmail.com>
2021-01-20 16:51:41 +0700

    fix tesst

--- core/tests.py
+++ core/tests.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from core import models
 
 RECEIPT_URL = reverse('core_api:receipt-list')
-TOKEN_URL = reverse('core_api:token')
 
 
 def create_user(**params):
@@ -27,39 +26,3 @@ class UserTests(APITestCase):
 
         return models.Receipt.objects.create(employer=user, material=material, **defaults)
 
-
-class ReceiptTests(APITestCase):
-
-    def setUp(self):
-        self.client = APIClient()
-
-        self.user = User.objects.create_user(
-            username="test-user",
-            password="123456"
-        )
-        self.client.force_authenticate(user=self.user)
-
-    def test_view_receipt(self):
-        response = self.client.get(RECEIPT_URL, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_create_basic_recipet(self):
-        """Test creating receipt"""
-
-        material = models.Material.objects.create(
-            title="Gạo", description="gao xay")
-
-        payload = {
-            "material": material.id,
-            "quantity": 3,
-            "total_cost": 2
-        }
-        res = self.client.post(RECEIPT_URL, payload)
-        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-
-        receipt = models.Receipt.objects.get(id=res.data['id'])
-        for key in payload.keys():
-            if key == "material":
-                self.assertEqual(material, getattr(receipt, key))
-            else:
-                self.assertEqual(payload[key], getattr(receipt, key))
