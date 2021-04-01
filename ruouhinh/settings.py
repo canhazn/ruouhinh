3c4f46295f5f3174d2c784d0cfda7416dbf22d5c
canhazn <canhazn@gmail.com>
2021-04-01 22:49:01 +0700

    new app

--- ruouhinh/settings.py
+++ ruouhinh/settings.py
@@ -178,7 +178,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=25),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3600),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
